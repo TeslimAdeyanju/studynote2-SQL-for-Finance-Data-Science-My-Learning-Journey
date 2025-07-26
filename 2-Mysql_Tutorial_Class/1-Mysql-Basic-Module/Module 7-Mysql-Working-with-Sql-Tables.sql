0625e79dace2108821a8772879528ec345eecbb8
Teslim Adeyanju <info@adeyanjuteslim.co.uk>
2025-07-26 18:37:55 +0100

    update on the code

--- 2-Mysql_Tutorial_Class/1-Mysql-Basic-Module/Module 7-Mysql-Working-with-Sql-Tables.sql
+++ 2-Mysql_Tutorial_Class/1-Mysql-Basic-Module/Module 7-Mysql-Working-with-Sql-Tables.sql
@@ -1,3 +1,6 @@
+
+//CREATE TABLE//
+
 DROP TABLE IF EXISTS students;
 
 CREATE TABLE students (
@@ -75,3 +78,152 @@ SHOW CREATE TABLE student_records
 
 ALTER TABLE student_records
 DROP INDEX unique_email;
+
+
+
+
+
+
+// Module- TEMPORARY TABLES 
+
+--Example 1
+
+--create a temporary table 
+CREATE TEMPORARY TABLE credits(
+  customerNumber INT PRIMARY KEY, 
+  creditLimit DEC(10, 2)
+);
+
+
+--insert into the temporary table 
+INSERT INTO credits(customerNumber, creditLimit)
+SELECT 
+  customerNumber, 
+  creditLimit 
+FROM 
+  customers 
+WHERE 
+  creditLimit > 0;
+
+-- query from the temporary table 
+SELECT * FROM credits;
+
+
+-- Example 2
+
+--create a temporary budget table 
+CREATE TEMPORARY TABLE budget(
+     productLine VARCHAR(50), 
+     orderYear YEAR,
+     orderValue DECIMAL(42,2)
+     );
+
+--insert into the budget table 
+insert into budget(productLine, orderYear, orderValue)
+     select productLine, 
+            orderYear, 
+            orderValue
+     from sales; 
+    
+--  view the insert data 
+select * from budget;        
+
+-- view only the year from the data budget 
+select orderyear
+from budget;          
+            
+       
+            
+            
+            
+            
+            
+            
+         
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
