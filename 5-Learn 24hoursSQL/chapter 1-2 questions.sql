13c86739fd6f1ea9646daa4df526243fa2e741ff
Teslim Adeyanju <info@adeyanjuteslim.co.uk>
2025-08-22 23:23:19 +0100

    update on learning sql

---
+++ 5-Learn 24hoursSQL/chapter 1-2 questions.sql
@@ -0,0 +1,14 @@
+/* chapter 1 -2: Retrieval data */
+
+--question 1
+select * 
+from customers;
+
+-- question 2
+select DISTINCT prod_id
+from orderitems
+limit 7;
+
+--question 3
+
+select * from customers
