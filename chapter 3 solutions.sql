e4a655824e0587124b763929cb0743029236c0dc
Teslim Adeyanju <info@adeyanjuteslim.co.uk>
2025-09-02 01:36:21 +0100

    question

--- chapter 3 solutions.sql
+++ chapter 3 solutions.sql
@@ -1,3 +1,23 @@
-select prod_id, prod_price, prod_name
-from products
-order by 2
+/* Write a SQL statement to retrieve all customer names (cust_names) from the Customers table, and display the results sorted from Z to A.*/
+
+-- ascending 
+select cust_name from customers
+order by cust_name desc;
+
+
+-- wothout descending 
+select cust_name from customers
+order by cust_name;
+
+
+-- questions 2
+select cust_id, order_num
+from orders 
+order by cust_id, order_num desc;
+
+
+/*question 3*/
+
+select quantity, item_price
+from OrderItems
+order by item_price;
