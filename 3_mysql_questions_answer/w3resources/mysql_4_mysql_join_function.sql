2d69819c3ad0385542b9a6edeaa3f757903a7825
Teslim Adeyanju <info@adeyanjuteslim.co.uk>
2026-06-16 19:42:29 +0100

    daily coding excercise done today of 3 questions

--- 3_mysql_questions_answer/w3resources/mysql_4_mysql_join_function.sql
+++ 3_mysql_questions_answer/w3resources/mysql_4_mysql_join_function.sql
@@ -10,8 +10,7 @@
          COUNTRY_NAME
       FROM HR_db.locations
       JOIN HR_db.countries
-      USING
-         (COUNTRY_ID)
+      USING (COUNTRY_ID)
    -- 2. Write a MySQL query to find the name (first_name, last name), department ID and name of
    -- all*/
    /*-- the employees.*/
@@ -20,8 +19,7 @@
          LAST_NAME
       FROM HR_db.employees
       JOIN HR_db.departments
-      USING
-         (DEPARTMENT_ID)
+      USING (DEPARTMENT_ID)
    /*-- 3. Write a MySQL query to find the name (first_name, last_name), job, department ID and name -- of*/
    /*-- the employees who works in London.*/
    SELECT
@@ -31,8 +29,7 @@
          departments.DEPARTMENT_ID
       FROM HR_db.employees
       JOIN HR_db.departments
-      USING
-         (DEPARTMENT_ID)
+      USING (DEPARTMENT_ID)
       JOIN HR_db.locations
       WHERE CITY = 'London'
    -- 4. Write a MySQL query to find the employee id, name (last_name) along with their manager_id and name (last_name).
@@ -43,8 +40,7 @@
          m.last_name
       FROM HR_db.employees AS e
       JOIN employees AS m
-      USING
-         MANAGER_ID
+      USING MANAGER_ID
       WHERE m.MANAGER_ID IS NOT NULL;
    -- 5. Write a MySQL query to find the name (first_name, last_name) and hire date of the employees who was hired after 'Jones'.
    SELECT
@@ -71,24 +67,71 @@
       FROM job_history
       JOIN jobs
       WHERE DEPARTMENT_ID = 90;
-      
-      -- 8. Write a MySQL query to display the department ID and name and first name of manager.
-      
-       SELECT
+   -- 8. Write a MySQL query to display the department ID and name and first name of manager.
+   SELECT
          e.FIRST_NAME,
-         e.MANAGER_ID, 
+         e.MANAGER_ID,
          d.DEPARTMENT_NAME
       FROM employees AS e
       JOIN HR_db.departments AS d ON e.DEPARTMENT_ID = d.DEPARTMENT_ID;
-   
+   -- 9. Write a MySQL query to display the department name, manager name, and city.
+   SELECT
+         d.DEPARTMENT_NAME,
+         e.FIRST_NAME,
+         CITY
+      FROM employees AS e
+      JOIN departments AS d ON e.EMPLOYEE_ID = d.MANAGER_ID
+      JOIN HR_db.locations
+      USING (location_id);
+   -- 10. Write a MySQL query to display the job title and average salary of employees.
+   SELECT
+         J.JOB_TITLE,
+         ROUND(AVG(e.SALARY),2) AS average_salary
+      FROM employees AS e
+      JOIN jobs AS J
+      USING (job_id)
+      GROUP BY
+         J.JOB_TITLE
+      ORDER BY
+         J.JOB_TITLE;
+   -- 11. Write a MySQL query to display job title, employee name, and the difference between salary of the employee and minimum salary for the job.
+   SELECT
+         J.Job_title,
+         CONCAT(E.FIRST_NAME, " ", E.LAST_NAME) AS full_name,
+         (E.SALARY - J.MIN_SALARY) AS diff_salary
+      FROM employees AS E
+      JOIN jobs AS J
+      USING (JOB_ID)
+      ORDER BY
+         full_name;
+         
+       
+       
+       
+      -- 12. Write a MySQL query to display the job history that were done by any employee who is currently drawing more than 10000 of salary.
+   SELECT
+         E.EMPLOYEE_ID,
+         E.FIRST_NAME,
+         j.START_DATE,
+         j.END_DATE,
+         E.JOB_ID,
+         E.DEPARTMENT_ID
+      FROM employees AS E
+      JOIN Job_history AS j
+      USING (EMPLOYEE_ID)
+      WHERE E.SALARY > 10000
+      ORDER BY E.DEPARTMENT_ID;
       
       
-      
-     
-     
-      
-
-
-
-
-
+   -- 13. Write a MySQL query to display department name, name (first_name, last_name), hire date,
+   -- salary of the manager for all managers whose experience is more than 15 years.
+   SELECT
+         D.DEPARTMENT_NAME,
+         CONCAT( E.FIRST_NAME, " ", E.LAST_NAME) AS name,
+         E.HIRE_DATE,
+         E.SALARY
+      FROM employees AS E
+      JOIN departments AS D
+      USING (DEPARTMENT_ID)
+ 
+ 
