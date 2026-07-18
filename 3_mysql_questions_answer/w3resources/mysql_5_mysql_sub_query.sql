b0e8dff592608ed57eaa5f80b7c41dc7d5457618
Teslim Adeyanju <info@adeyanjuteslim.co.uk>
2026-07-18 22:52:27 +0100

    Daily practice of SQL for improvement

---
+++ 3_mysql_questions_answer/w3resources/mysql_5_mysql_sub_query.sql
@@ -0,0 +1,15 @@
+SELECT 
+    first_name, 
+    last_name, 
+    salary
+FROM 
+    employees
+WHERE 
+    salary >
+    (   SELECT 
+            salary
+        FROM 
+            employees
+        WHERE 
+            last_name ='Bull');
+
